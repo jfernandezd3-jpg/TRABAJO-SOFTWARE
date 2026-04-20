cce15fa57bb0569c43915cd246be77ee37c45e56
aamunarrizm <aamunarrizm@alumni.unav.es>
2026-04-20 12:33:53 +0200

    ander_ales

--- WEB-INF/classes/ManageParticipantsServlet.java
+++ WEB-INF/classes/ManageParticipantsServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/ManageParticipantsServlet")
+
 public class ManageParticipantsServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
